3bef039ec3fc02a633766739ec924967621b9e08
Colbyn Wadman <hello@colbyn.com>
2024-03-30 17:18:08 -0600

    update Package meta

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "PrettyTree",
+    name: "SwiftPrettyTree",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library( name: "PrettyTree", targets: ["PrettyTree"] ),
