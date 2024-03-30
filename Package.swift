d1d7692907720ba19ee28ab8050d2f1adbe1dfc0
Colbyn Wadman <hello@colbyn.com>
2024-03-30 17:22:29 -0600

    update package meta

--- Package.swift
+++ Package.swift
@@ -7,12 +7,12 @@ let package = Package(
     name: "SwiftPrettyTree",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library( name: "PrettyTree", targets: ["PrettyTree"] ),
+        .library( name: "SwiftPrettyTree", targets: ["SwiftPrettyTree"] ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target( name: "PrettyTree" ),
+        .target( name: "SwiftPrettyTree" ),
 //        .testTarget( name: "PrettyTreeTests", dependencies: ["PrettyTree"] ),
     ]
 )
